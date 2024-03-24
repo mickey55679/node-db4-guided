eb55c86ade7b47c028e0344b5531c768637b13fc
Michaiah Bos <michaiahbos@yahoo.com>
2024-03-24 17:42:54 -0500

    wip

---
+++ data/migrations/20240324223940_first-migration.js
@@ -0,0 +1,24 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = async function(knex) {
+  await knex.schema
+  .createTable()
+  .createTable()
+  .createTable()
+  .createTable()
+
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = async function(knex) {
+  await knex.schema
+  .dropTableIfExists()
+  .dropTableIfExists()
+  .dropTableIfExists()
+  .dropTableIfExists()
+};
